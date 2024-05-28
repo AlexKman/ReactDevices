a181f9ab464ad22d2a19cfab3fc3aa8e1a9769f2
Alex <keeley.lp@gmail.com>
2024-05-28 16:11:49 +0100

    Updated formatHeader helper to add spaces before caps and then return with first character capitalised

--- src/helpers/formatHeader.ts
+++ src/helpers/formatHeader.ts
@@ -1,3 +1,7 @@
 export function formatHeader(header: string) {
-  return header;
+  // Add spaces before every upper case
+  let headerWithSpaces = header.replace(/([A-Z])/g, " $1");
+
+  // Return with first character in string capitalised e.g last Report Time -> Last Report Time
+  return headerWithSpaces.charAt(0).toUpperCase() + headerWithSpaces.slice(1);
 }
