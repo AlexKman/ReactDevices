8d3b6bc0e52656c0000e81bef20acb4599d4245e
Alex <keeley.lp@gmail.com>
2024-05-28 16:04:27 +0100

    Using useState to setData from api response in useEffect, mapping over data in bootstrap table to display rows, headers currently fixed

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Homepage() {
+  const [dataArr, setData] = useState<any[]>([]);
+
   useEffect(() => {
     axios
       .get(
         "https://pzv500llz9.execute-api.eu-west-2.amazonaws.com/production/listDevices"
       )
       .then((response) => {
-        console.log(response, "test response");
+        setData(response.data.results);
       })
       .catch((error) => {
         console.log(error);
@@ -16,8 +18,30 @@ export default function Homepage() {
   }, []);
 
   return (
-    <div className="homepage">
-      <h1>Devices</h1>
+    <div className="homepage ">
+      <h1 className="align-self-center">Devices</h1>
+      <table className="homepage-table table">
+        <thead>
+          <tr>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataArr.map((data) => (
+            <tr className="table-row">
+              <td>{data.id}</td>
+              <td>{data.name ?? "Unknown"}</td>
+              <td>{data.model?.name}</td>
+              <td>{data.lastReportTime}</td>
+              <td>{data.nextReportTime}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
